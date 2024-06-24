eda98ab2b75fe9ae02439a9574a9ee2bb091d35e
mdShams63 <123280275+mdShams63@users.noreply.github.com>
2024-06-24 22:24:48 +0600

    Tik Tac Toe.cpp

--- Tik Tac Toe.cpp
+++ Tik Tac Toe.cpp
@@ -1,126 +1,104 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 void printBoard();
 int checkwin();
 
-char board[]= {'0','1','2','3','4','5','6','7','8','9'};
+char board[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-void main()
-{
-    int player=1,input,status=-1;
+int main() {
+    int player = 1, input, status = -1;
     printBoard();
 
-    while(status==-1)
-    {
-       if (player % 2 == 0) {
-    player = 2;
-} else {	
-    player = 1;
-}
+    while (status == -1) {
+        if (player % 2 == 0) {
+            player = 2;
+        } else {
+            player = 1;
+        }
 
         char mark;
 
-if (player == 1) {
-    mark = 'x';
-} else {
-    mark = '0';
-}
-        printf(" Dear Player %d\n Enter a number :\n",player);
-        scanf("%d", &input);
-        if(input<1 || input>9)
-        {
-            printf("invalid input");
+        if (player == 1) {
+            mark = 'x';
+        } else {
+            mark = '0';
+        }
+        cout << " Dear Player " << player << "\n Enter a number:\n";
+        cin >> input;
+
+        if (input < 1 || input > 9) {
+            cout << "Invalid input";
+            continue;
         }
 
-        board[input]=mark;
+        board[input] = mark;
         printBoard();
 
-        int result=checkwin();
+        int result = checkwin();
 
-        if(result==1)
-        {
-            printf("Congratulation! Player %d. You are the winner,yahoo!", player);
-            return;
-        }
-        else if(result==0)
-        {
-            printf("It's a Draw. Better Luck Next Time");
-            return;
+        if (result == 1) {
+            cout << "Congratulations! Player " << player << ". You are the winner, yahoo!\n";
+            return 0;
+        } else if (result == 0) {
+            cout << "It's a Draw. Better Luck Next Time\n";
+            return 0;
         }
         player++;
     }
+    return 0;
 }
 
-void printBoard()
-{
+void printBoard() {
+    // system("cls"); // Uncomment this line for Windows to clear the screen
     system("cls");
-    printf("\n\n");
-    printf("\t\t          TIC TAC TOE \t\t \n\n");
-    printf("\t\t Dear Player, please select a number \n");
-    printf("\n\n");
-    printf("\t\t\t    %c   %c   %c \n",board[1],board[2],board[3]);
-    printf("\n");
-    printf("\n");
-    printf("\t\t\t    %c   %c   %c \n",board[4],board[5],board[6]);
-    printf("\n");
-    printf("\n");
-    printf("\t\t\t    %c   %c   %c \n",board[7],board[8],board[9]);
-    printf("\n");
-    printf("\n\n");
-
+    cout << "\n\n";
+    cout << "\t\t          TIC TAC TOE \t\t \n\n";
+    cout << "\t\t Dear Player, please select a number \n";
+    cout << "\n\n";
+    cout << "\t\t\t    " << board[1] << "   " << board[2] << "   " << board[3] << " \n";
+    cout << "\n";
+    cout << "\t\t\t    " << board[4] << "   " << board[5] << "   " << board[6] << " \n";
+    cout << "\n";
+    cout << "\t\t\t    " << board[7] << "   " << board[8] << "   " << board[9] << " \n";
+    cout << "\n\n";
 }
 
-
-int checkwin()
-{
-    if(board[1]==board[2] && board[2]==board[3])
-    {
+int checkwin() {
+    if (board[1] == board[2] && board[2] == board[3]) {
         return 1;
     }
-    if(board[1]==board[4] && board[4]==board[7])
-    {
+    if (board[1] == board[4] && board[4] == board[7]) {
         return 1;
     }
-    if(board[7]==board[8] && board[8]==board[9])
-    {
+    if (board[7] == board[8] && board[8] == board[9]) {
         return 1;
     }
-    if(board[3]==board[6] && board[6]==board[9])
-    {
+    if (board[3] == board[6] && board[6] == board[9]) {
         return 1;
     }
-    if(board[1]==board[5] && board[5]==board[9])
-    {
+    if (board[1] == board[5] && board[5] == board[9]) {
         return 1;
     }
-    if(board[3]==board[5] && board[5]==board[7])
-    {
+    if (board[3] == board[5] && board[5] == board[7]) {
         return 1;
     }
-    if(board[2]==board[5] && board[5]==board[8])
-    {
+    if (board[2] == board[5] && board[5] == board[8]) {
         return 1;
     }
-    if(board[4]==board[5] && board[5]==board[6])
-    {
+    if (board[4] == board[5] && board[5] == board[6]) {
         return 1;
     }
 
-    int count=0;
-    for(int i = 1; i <= 9; i++)
-    {
-        if(board[i]=='x' || board[i]=='0')
-        {
+    int count = 0;
+    for (int i = 1; i <= 9; i++) {
+        if (board[i] == 'x' || board[i] == '0') {
             count++;
         }
     }
 
-    if(count==9)
-    {
+    if (count == 9) {
         return 0;
     }
     return -1;
 }
-
-
-
